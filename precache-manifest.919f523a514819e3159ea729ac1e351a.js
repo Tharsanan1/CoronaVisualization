52eaf08611f5c9603da692475651e1a668d5a6ed
tharsanan1 <tharsanan.15@cse.mrt.ac.lk>
2020-03-20 23:55:08 +0530

    Updates

---
+++ precache-manifest.919f523a514819e3159ea729ac1e351a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae6fa04ad5cc0f0f6b34ca45822302c8",
+    "url": "/CoronaVisualization/index.html"
+  },
+  {
+    "revision": "d3efbe1582cc59425287",
+    "url": "/CoronaVisualization/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "4751213bb227ec21873f",
+    "url": "/CoronaVisualization/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3efbe1582cc59425287",
+    "url": "/CoronaVisualization/static/js/2.6add0bfa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/CoronaVisualization/static/js/2.6add0bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4751213bb227ec21873f",
+    "url": "/CoronaVisualization/static/js/main.985303a5.chunk.js"
+  },
+  {
+    "revision": "1f888a733e4b47c586dd",
+    "url": "/CoronaVisualization/static/js/runtime-main.63254853.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CoronaVisualization/static/media/logo.5d5d9eef.svg"
+  }
+]);
